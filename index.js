1d3f974ad7bdc1307a85c5b1a1e0269418d8b7df
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-21 00:30:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function breakOut(array, changeValue, stopValue) {
-  for (var i = 0; l = array.length; i < l; i++) {
+  for (var i = 0; length = array.length; i < length; i++) {
     if (array[i] === stopValue) {
       break;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; length = array.length; i < length; i++) {
+    if (array[i] === skipValue) {
+      continue;
     }
     array[i] = changeValue;
   }
